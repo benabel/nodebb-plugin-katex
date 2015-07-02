e37e415024acaf2f4625e4f2ff3de9ea7d1b1a81
Benjamin ABEL <bbig26@gmail.com>
2015-07-02 23:35:04 +0200

    Fix multiple math rendering in stripped tags summaries

--- public/js/client.js
+++ public/js/client.js
@@ -3,60 +3,30 @@
 
 $(document).ready(function() {
 
-	//$(window).on('action:composer.preview', {
-	//	selector: '.composer .preview'
-	//}, Markdown.highlight);
+  // use only mathml in stripped tags summary
+  $("annotation").remove();
+  $("katex").contents().filter(function() {
+    return this.nodeType === 3;
+  }).remove();
 
-	require(['composer/formatting', 'composer/controls', 'components'], function(formatting, controls, components) {
+  //$(window).on('action:composer.preview', {
+  //	selector: '.composer .preview'
+  //}, Markdown.highlight);
 
-		$(window).on('action:posts.loaded action:topic.loaded action:posts.edited', function() {
-			//Markdown.highlight(components.get('post/content').find('pre code'));
-		});
+  require(['composer/formatting', 'composer/controls', 'components'], function(formatting, controls, components) {
 
-		formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd){
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '$inline maths$');
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '$');
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
-			}
-		});
+    $(window).on('action:posts.loaded action:topic.loaded action:posts.edited', function() {
+      //Markdown.highlight(components.get('post/content').find('pre code'));
+    });
 
-		formatting.addButtonDispatch('italic', function(textarea, selectionStart, selectionEnd){
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, "*italicised text*");
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 16);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '*');
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
-			}
-		});
-
-		formatting.addButtonDispatch('list', function(textarea, selectionStart, selectionEnd){
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, "\n* list item");
-
-				// Highlight "list item"
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 12);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '\n* ', '');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
-			}
-		});
-
-		formatting.addButtonDispatch('link', function(textarea, selectionStart, selectionEnd){
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, "[link text](link url)");
-
-				// Highlight "link url"
-				controls.updateTextareaSelection(textarea, selectionStart + 12, selectionEnd + 20);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '[', '](link url)');
-
-				// Highlight "link url"
-				controls.updateTextareaSelection(textarea, selectionEnd + 3, selectionEnd + 11);
-			}
-		});
-	});
+    formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
+      if (selectionStart === selectionEnd) {
+        controls.insertIntoTextarea(textarea, '$inline maths$');
+        controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
+      } else {
+        controls.wrapSelectionInTextareaWith(textarea, '$');
+        controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
+      }
+    });
+  });
 });
