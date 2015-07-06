99e50accd1c2aa20bb2535cb16d58add8f0ad948
Benjamin ABEL <bbig26@gmail.com>
2015-07-06 20:48:47 +0200

    Beautify files with new rc

--- library.js
+++ library.js
@@ -1,108 +1,108 @@
-(function() {
-    "use strict";
+(function () {
+	"use strict";
 
-  var fs = require('fs'),
-    path = require('path'),
-    url = require('url'),
-    katex = require('katex'),
-    mathRenderer = require('./static/lib/auto-render/auto-render'),
-    meta = module.parent.require('./meta'),
-    nconf = module.parent.require('nconf'),
-    plugins = module.parent.exports,
-    parser,
-    delimiters = [{
-      left: "$$",
-      right: "$$",
-      display: true
-    }, {
-      left: "\\[",
-      right: "\\]",
-      display: true
-    }, {
-      left: "\\(",
-      right: "\\)",
-      display: false
-    }],
-    Katex = {
-      config: {},
-      onLoad: function(params, callback) {
-        function render(req, res, next) {
-          res.render('admin/plugins/katex');
-        }
+	var fs = require('fs'),
+		path = require('path'),
+		url = require('url'),
+		katex = require('katex'),
+		mathRenderer = require('./static/lib/auto-render/auto-render'),
+		meta = module.parent.require('./meta'),
+		nconf = module.parent.require('nconf'),
+		plugins = module.parent.exports,
+		parser,
+		delimiters = [{
+			left: "$$",
+			right: "$$",
+			display: true
+		}, {
+			left: "\\[",
+			right: "\\]",
+			display: true
+		}, {
+			left: "\\(",
+			right: "\\)",
+			display: false
+		}],
+		Katex = {
+			config: {},
+			onLoad: function (params, callback) {
+				function render(req, res, next) {
+					res.render('admin/plugins/katex');
+				}
 
-        params.router.get('/admin/plugins/katex', params.middleware.admin.buildHeader, render);
-        params.router.get('/api/admin/plugins/katex', render);
-        Katex.init();
-        callback();
-      },
+				params.router.get('/admin/plugins/katex', params.middleware.admin.buildHeader, render);
+				params.router.get('/api/admin/plugins/katex', render);
+				Katex.init();
+				callback();
+			},
 
-      init: function() {
-        // Load saved config
-        var _self = this,
-          fields = ['dollarInline'],
-          defaults = {
-            'dollarInline': false
-          };
+			init: function () {
+				// Load saved config
+				var _self = this,
+					fields = ['dollarInline'],
+					defaults = {
+						'dollarInline': false
+					};
 
-        meta.settings.get('katex', function(err, options) {
-          for (var field in defaults) {
-            // If not set in config (nil)
-            if (!options.hasOwnProperty(field)) {
-              _self.config[field] = defaults[field];
-            } else {
-              _self.config[field] = options[field];
-            }
-          }
-          // manage dollar parsing from admincontrol panel
-          if (_self.config.dollarInline === 'on' && delimiters.length < 4) {
-            delimiters.push({
-              left: "$",
-              right: "$",
-              display: false
-            });
-          } else {
-            delimiters.slice(0, 2);
-          }
-        });
-      },
+				meta.settings.get('katex', function (err, options) {
+					for (var field in defaults) {
+						// If not set in config (nil)
+						if (!options.hasOwnProperty(field)) {
+							_self.config[field] = defaults[field];
+						} else {
+							_self.config[field] = options[field];
+						}
+					}
+					// manage dollar parsing from admincontrol panel
+					if (_self.config.dollarInline === 'on' && delimiters.length < 4) {
+						delimiters.push({
+							left: "$",
+							right: "$",
+							display: false
+						});
+					} else {
+						delimiters.slice(0, 2);
+					}
+				});
+			},
 
-      parsePost: function(data, callback) {
-        if (data && data.postData && data.postData.content) {
-          data.postData.content = mathRenderer.render(data.postData.content, delimiters);
-        }
-        callback(null, data);
-      },
+			parsePost: function (data, callback) {
+				if (data && data.postData && data.postData.content) {
+					data.postData.content = mathRenderer.render(data.postData.content, delimiters);
+				}
+				callback(null, data);
+			},
 
-      parseRaw: function(raw, callback) {
-        callback(null, raw ? mathRenderer.render(raw, delimiters) : raw);
-      },
+			parseRaw: function (raw, callback) {
+				callback(null, raw ? mathRenderer.render(raw, delimiters) : raw);
+			},
 
-      registerFormatting: function(payload, callback) {
-        var formatting = ['usd'];
+			registerFormatting: function (payload, callback) {
+				var formatting = ['usd'];
 
-        formatting.reverse();
-        formatting.forEach(function(format) {
-          payload.options.unshift({
-            name: format,
-            className: 'fa fa-' + format
-          });
-        });
+				formatting.reverse();
+				formatting.forEach(function (format) {
+					payload.options.unshift({
+						name: format,
+						className: 'fa fa-' + format
+					});
+				});
 
-        callback(null, payload);
-      },
+				callback(null, payload);
+			},
 
-      admin: {
-        menu: function(custom_header, callback) {
-          custom_header.plugins.push({
-            "route": '/plugins/katex',
-            "icon": 'fa-usd',
-            "name": 'katex'
-          });
+			admin: {
+				menu: function (custom_header, callback) {
+					custom_header.plugins.push({
+						"route": '/plugins/katex',
+						"icon": 'fa-usd',
+						"name": 'katex'
+					});
 
-          callback(null, custom_header);
-        }
-      }
-    };
+					callback(null, custom_header);
+				}
+			}
+		};
 
-  module.exports = Katex;
+	module.exports = Katex;
 })();
--- static/js/client.js
+++ static/js/client.js
@@ -1,26 +1,26 @@
-(function() {
-  "use strict";
+(function () {
+	"use strict";
 
-  $(document).ready(function() {
+	$(document).ready(function () {
 
-    // use only mathml in stripped tags summary
-    $("div.post-preview-content annotation").remove();
-    $("katex").contents().filter(function() {
-      return this.nodeType === 3;
-    }).remove();
+		// use only mathml in stripped tags summary
+		$("div.post-preview-content annotation").remove();
+		$("katex").contents().filter(function () {
+			return this.nodeType === 3;
+		}).remove();
 
-    // add the usd button to the composer
-    require(['composer/formatting', 'composer/controls', 'components'], function(formatting, controls, components) {
+		// add the usd button to the composer
+		require(['composer/formatting', 'composer/controls', 'components'], function (formatting, controls, components) {
 
-      formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
-        if (selectionStart === selectionEnd) {
-          controls.insertIntoTextarea(textarea, '$inline maths$');
-          controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
-        } else {
-          controls.wrapSelectionInTextareaWith(textarea, '$');
-          controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
-        }
-      });
-    });
-  });
+			formatting.addButtonDispatch('usd', function (textarea, selectionStart, selectionEnd) {
+				if (selectionStart === selectionEnd) {
+					controls.insertIntoTextarea(textarea, '$inline maths$');
+					controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
+				} else {
+					controls.wrapSelectionInTextareaWith(textarea, '$');
+					controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
+				}
+			});
+		});
+	});
 })();
