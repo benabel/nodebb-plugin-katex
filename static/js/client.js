15ef9f16209ca7e7b5f2845f59a70adccaf32e64
Benjamin Abel <bbig26@gmail.com>
2017-03-18 15:38:18 +0100

    Switch to airbnb style

--- static/js/client.js
+++ static/js/client.js
@@ -13,31 +13,29 @@ $(document).ready(function() {
   * Add a dollar button to the composer
   */
   Katex.prepareFormattingTools = function() {
-    require(
-        ['composer/formatting', 'composer/controls'],
-        function(formatting, controls) {
-          if (Katex.dollarInline === 'on') {
-            formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
-              if (selectionStart === selectionEnd) {
-                controls.insertIntoTextarea(textarea, '$inline maths$');
-                controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
-              } else {
-                controls.wrapSelectionInTextareaWith(textarea, '$');
-                controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
-              }
-            });
+    require(['composer/formatting', 'composer/controls'], function(formatting, controls) {
+      if (Katex.dollarInline === 'on') {
+        formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
+          if (selectionStart === selectionEnd) {
+            controls.insertIntoTextarea(textarea, '$inline maths$');
+            controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 13);
           } else {
-            formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
-              if (selectionStart === selectionEnd) {
-                controls.insertIntoTextarea(textarea, '\\\\(inline maths\\\\)');
-                controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 15);
-              } else {
-                controls.wrapSelectionInTextareaWith(textarea, '\\\\(', '\\\\)');
-                controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
-              }
-            });
+            controls.wrapSelectionInTextareaWith(textarea, '$');
+            controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 1);
           }
         });
+      } else {
+        formatting.addButtonDispatch('usd', function(textarea, selectionStart, selectionEnd) {
+          if (selectionStart === selectionEnd) {
+            controls.insertIntoTextarea(textarea, '\\\\(inline maths\\\\)');
+            controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 15);
+          } else {
+            controls.wrapSelectionInTextareaWith(textarea, '\\\\(', '\\\\)');
+            controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
+          }
+        });
+      }
+    });
   };
 
   // use only mathml in stripped tags summary
